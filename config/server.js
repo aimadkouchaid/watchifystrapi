fe775c10e8c5c8de1db29d47734aafc2e24813be
unknown <coderella.dev@gmail.com>
2023-09-15 17:52:43 +0100

    commit

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['6SAwh5kukLWfJV/vL5JhZw==', 'L2JK5IE3FrueQCXKMD83ew=='],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
